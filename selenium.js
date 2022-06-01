f4fee193110f5ef93b1bc20c1ec7fef5b7411c14
eddyholmes <eddy@webdevgroup.com>
2022-06-01 12:37:46 +0500

    add confi

--- selenium.js
+++ selenium.js
@@ -1,17 +1,16 @@
 const { remote } = require('webdriverio');
 
 (async () => {
-    const browser = await remote({
-        capabilities: {
-            browserName: 'chrome'
-        },
-    })
-
-    await browser.url('https://webdriver.io')
-
-    const apiLink = await browser.$('=API')
-    await apiLink.click()
-
-    await browser.saveScreenshot('./screenshot.png')
-    await browser.deleteSession()
+  const browser = await remote({
+    capabilities: {
+      browserName: 'chrome'
+    },
+    hostname: "selenium-ch",
+    port: 4444
+  })
+  await browser.url('https://webdriver.io')
+  const apiLink = await browser.$('=API')
+  await apiLink.click()
+  await browser.saveScreenshot('./screenshot.png')
+  await browser.deleteSession()
 })()
